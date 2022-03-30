c293ce08c72543b0f6aaa22848c2b8d35b6ad531
dang diep <ddiep@gmail.com>
2022-03-30 17:36:25 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,25 @@
+package basic.dev;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+       String nameStudent = "Nguyen Van A";
+       String codeStudent = "12345";
+       String identityCard = "12345";
+       String nameClass = "12A";
+       String nameSchool = " Nguyen Hue";
+       float mark= 9;
+
+       String nameWorker = "Nguyen Van B";
+       String address = "2 Le Loi";
+       String phoneNumber = "0123455689";
+       float salary= 2.3f;
+
+       String nameCar = "VF 8";
+       String nameCompany = "VinFast";
+       double price = 345678898;
+       String color = "red";
+
+    }
+}
